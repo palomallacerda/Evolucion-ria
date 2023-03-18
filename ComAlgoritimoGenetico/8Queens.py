23fbff434ee86da11a5dba99270d2291dcad4030
palomallacerda <psls@ic.ufal.br>
2023-03-18 11:59:50 -0300

    updating

--- ComAlgoritimoGenetico/8Queens.py
+++ ComAlgoritimoGenetico/8Queens.py
@@ -1,6 +1,10 @@
+#Problema das 8 rainhas com as taxas de cruzamento e variação fixos. 
+# Printa o melhor resul
+
 import random
 import matplotlib.pyplot as plt
 
+# Função para visualizar o tabuleiro
 def view(board):
     for i in range(8):
         for j in range(8):
@@ -19,7 +23,7 @@ def fitness(board):
                 attacks += 1
     return 28 - attacks
 
-
+# Função para realizar o cruzamento entre dois tabuleiros
 def crossover(board1, board2, crossover_rate):
     if random.random() < crossover_rate:
         split_point = random.randint(1, 7)
@@ -35,15 +39,13 @@ def mutate(board, mutation_rate):
         index = random.randint(0, 7)
         new_value = random.randint(0, 7)
         board[index] = new_value
-    return 
+    return board
 
+# Parâmetros do algoritmo genético
 population_size = 100
-crossover_rate_min = 0.6
-crossover_rate_max = 0.9
-mutation_rate_min = 0.05
-mutation_rate_max = 0.2
+crossover_rate = 0.8
+mutation_rate = 0.1
 generations = 70
-
 
 # Inicialização da população
 population = []
@@ -51,39 +53,34 @@ for i in range(population_size):
     board = [random.randint(0, 7) for j in range(8)]
     population.append(board)
 
+# Execução do algoritmo genético
 max_fitness = []
 for generation in range(generations):
     # Avaliação do fitness da população
     fitness_scores = [fitness(board) for board in population]
     max_fitness.append(max(fitness_scores))
-
-# Seleção dos pais (torneio de dois)
-parents = []
-for i in range(population_size):
-    p1 = random.randint(0, population_size-1)
-    p2 = random.randint(0, population_size-1)
-    if fitness_scores[p1] > fitness_scores[p2]:
-        parents.append(population[p1])
-    else:
-        parents.append(population[p2])
-
-# Cruzamento e mutação para gerar nova população
-new_population = []
-for i in range(int(population_size/2)):
-    p1 = i * 2
-    p2 = i * 2 + 1
     
-    # Seleciona taxas de cruzamento e mutação aleatoriamente para cada par de pais
-    crossover_rate = random.uniform(crossover_rate_min, crossover_rate_max)
-    mutation_rate = random.uniform(mutation_rate_min, mutation_rate_max)
+    # Seleção dos pais (torneio de dois)
+    parents = []
+    for i in range(population_size):
+        p1 = random.randint(0, population_size-1)
+        p2 = random.randint(0, population_size-1)
+        if fitness_scores[p1] > fitness_scores[p2]:
+            parents.append(population[p1])
+        else:
+            parents.append(population[p2])
     
-    offspring1, offspring2 = crossover(parents[p1], parents[p2], crossover_rate)
-    offspring1 = mutate(offspring1, mutation_rate)
-    offspring2 = mutate(offspring2, mutation_rate)
-    new_population.append(offspring1)
-    new_population.append(offspring2)
-population = new_population
-
+    # Cruzamento e mutação para gerar nova população
+    new_population = []
+    for i in range(int(population_size/2)):
+        p1 = i * 2
+        p2 = i * 2 + 1
+        offspring1, offspring2 = crossover(parents[p1], parents[p2], crossover_rate)
+        offspring1 = mutate(offspring1, mutation_rate)
+        offspring2 = mutate(offspring2, mutation_rate)
+        new_population.append(offspring1)
+        new_population.append(offspring2)
+    population = new_population
 
 # Impressão da melhor solução encontrada
 best_board = max(population, key=fitness)
